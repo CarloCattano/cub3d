313d1b6792319b3334f68729e9841310b8eda47f
Carlo Cattano <ccattano@student.42berlin.de>
2023-10-08 19:38:31 +0200

    second

--- src/hooks.c
+++ src/hooks.c
@@ -15,13 +15,9 @@ int	motion_hook(int x, int y, t_data *d)
 int	mouse_scaling_hook(int k, int x, int y, t_data *d)
 {
 	(void)d;
-	if ((k == 1 || k == 2 || k == SCROLL_UP || k == SCROLL_DOWN)
+	if ((k == 1 || k == 2)
 		&& (x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT))
 	{
-		if (k == SCROLL_UP)
-		{}
-		else if (k == SCROLL_DOWN)
-		{}
 		if (k == MOUSE_LEFT || k == MOUSE_RIGHT)
 		{
 		}
