8d6d112205ff4e6e03edf8a515e111dc89d7b724
CarloCattano <freevibes@gmail.com>
2023-11-24 19:51:29 +0100

    clean tests

--- anim_test/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-
-cc -o your_program_name main.c weapon_animation.c -lmlx -L../minilibx-linux -I./minilibx-linux -I../inc/ -lft -L../libft -I../libft/inc/ -L./ -I. -lXext -lX11 -lm -lz -ggdb3 
-
