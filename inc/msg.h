16d8d94caff9f60777bcba946efbbb2e82383799
CarloCattano <freevibes@gmail.com>
2023-11-27 00:26:37 +0100

    fixed spelling

--- inc/msg.h
+++ inc/msg.h
@@ -6,7 +6,7 @@
 /*   By: carlo <carlo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 10:10:47 by jstrotbe          #+#    #+#             */
-/*   Updated: 2023/11/26 19:16:28 by jstrotbe         ###   ########.fr       */
+/*   Updated: 2023/11/27 00:25:23 by carlo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,17 @@
 # define E_ARG "CUBE3D NEEDS ONLY ONE ARG"
 
 /* PARSER */
-# define E_PARS "PARSING FAILD"
+# define E_PARS "PARSING FAILED"
 
 /*GENERAL FILE */
 # define E_FILE "CUB FILE CORRUPTED" 
-# define E_CUB "FILE NEED .CUB EXTENSION"
+# define E_CUB "FILE NEEDS .CUB EXTENSION"
 # define E_FO "OPEN CUB FILE FAIL"
 # define E_FC "FILE IS NOT RIGHT CONFIGURED"
 
 /* MSG CUB FILE */
 # define E_NE "NO EXTRAS SURPORTED IN THIS MODE"		
-# define E_SDT ".CUB HAVE DUPLICATE TYPES "
+# define E_SDT ".CUB HAS DUPLICATE TYPES "
 # define E_SARG ".CUB TYPE HAS NOT THE RIGHT NUMBER OF ARG"
 # define E_SWARG ".CUB TYPE ARG IS WRONG"		
 # define E_SDC ".CUB HAS DUPLICATE COLOUR "
@@ -44,10 +44,10 @@
 /* MAP */
 # define E_MF "MAP IS NOT CORRECT"
 # define E_NP "NO PLAYER"
-# define E_MP "MULTIPLE PLAYER"
-# define E_MW "MAP NOT CLOESD BY WALLS"
+# define E_MP "MULTIPLE PLAYERS"
+# define E_MW "MAP NOT ENCLOSED BY WALLS"
 
 /* INIT */
-# define E_IN "INIT FAILOR"
+# define E_IN "INIT FAILIURE"
 # define E_WL "LOAD WEAPON FAIL"
 #endif
