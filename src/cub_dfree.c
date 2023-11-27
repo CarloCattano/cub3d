a3b14a1d4294403abd66f4d45cc4cae20f1a57ef
Carlo Cattano <ccattano@student.42berlin.de>
2023-11-27 18:12:17 +0100

    add screenshot

--- src/cub_dfree.c
+++ src/cub_dfree.c
@@ -6,11 +6,14 @@
 /*   By: jstrotbe <jstrotbe@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 14:20:20 by jstrotbe          #+#    #+#             */
-/*   Updated: 2023/11/25 11:58:08 by jstrotbe         ###   ########.fr       */
+/*   Updated: 2023/11/27 16:41:22 by ccattano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+#include <math.h>
+
 
 void	cub_dfree(char ***tofree)
 {
